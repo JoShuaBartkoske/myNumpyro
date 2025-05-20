9af5e2faa08817b7053da62ca5d1f7c4f061e769
JoShuaBartkoske <shua4347@gmail.com>
2025-05-20 14:56:59 -0600

    create 8schools example from numpyro website

---
+++ 8schools.py
@@ -0,0 +1,41 @@
+'''
+Following along with the 8 schools exmample on numpyro's website: https://num.pyro.ai/en/latest/getting_started.html
+'''
+
+import numpy as np
+
+# set up the data:
+## there are 8 schools
+J = 8
+## each with their own SAT score treatment effects due to coaching and their individual standard deviation
+y = np.array([28.0, 8.0, -3.0, 7.0, -1.0, 1.0, 18.0, 12.0])
+sigma = np.array([15.0, 10.0, 16.0, 11.0, 9.0, 11.0, 10.0, 18.0])
+
+import numpyro
+
+import numpyro.distributions as dist
+
+def eight_schools(J, sigma, y=None):
+    mu = numpyro.sample('mu', dist.Normal(0, 5))
+    tau = numpyro.sample('tau', dist.HalfCauchy(5))
+    with numpyro.plate('J', J):
+        theta = numpyro.sample('theta', dist.Normal(mu, tau))
+        numpyro.sample('obs', dist.Normal(theta, sigma), obs=y)
+
+from jax import random
+
+from numpyro.infer import MCMC, NUTS
+
+nuts_kernel = NUTS(eight_schools)
+mcmc = MCMC(nuts_kernel, num_warmup=500, num_samples=1000)
+rng_key = random.PRNGKey(0)
+mcmc.run(rng_key, J, sigma, y=y, extra_fields=('potential_energy',))
+mcmc.print_summary()
+
+pe = mcmc.get_extra_fields()['potential_energy']
+print('Expected log joint density: {:.2f}'.format(np.mean(-pe))) 
+
+import arviz as az
+
+data = az.from_numpyro(mcmc)
+az.plot_trace(data, compact=True, figsize=(15, 25))
