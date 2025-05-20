a4a175874051681e42276e4fe096db203f39b1b7
JoShuaBartkoske <shua4347@gmail.com>
2025-05-20 16:12:29 -0600

    work on the reparameterized part of the 8 schools example

--- 8schools.py
+++ 8schools.py
@@ -12,7 +12,6 @@ y = np.array([28.0, 8.0, -3.0, 7.0, -1.0, 1.0, 18.0, 12.0])
 sigma = np.array([15.0, 10.0, 16.0, 11.0, 9.0, 11.0, 10.0, 18.0])
 
 import numpyro
-
 import numpyro.distributions as dist
 
 def eight_schools(J, sigma, y=None):
@@ -35,7 +34,30 @@ mcmc.print_summary()
 pe = mcmc.get_extra_fields()['potential_energy']
 print('Expected log joint density: {:.2f}'.format(np.mean(-pe))) 
 
-import arviz as az
+from numpyro.infer.reparam import TransformReparam
 
-data = az.from_numpyro(mcmc)
-az.plot_trace(data, compact=True, figsize=(15, 25))
+def eight_schools_noncentered(J, sigma, y=None):
+    mu = numpyro.sample('mu', dist.Normal(0, 5))
+    tau = numpyro.sample('tau', dist.HalfCauchy(5))
+    with numpyro.plate('J', J):
+        with numpyro.handlers.reparam(config={'theta': TransformReparam()}):
+
+            theta = numpyro.sample(
+                'theta',
+                dist.TransformedDistribution(dist.Normal(0., 1.),
+                                              dist.transforms.AffineTransform(mu, tau)))
+
+        numpyro.sample('obs', dist.Normal(theta, sigma), obs=y)
+
+nuts_kernel = NUTS(eight_schools_noncentered)
+mcmc = MCMC(nuts_kernel, num_warmup=500, num_samples=1000)
+rng_key = random.PRNGKey(0)
+mcmc.run(rng_key, J, sigma, y=y, extra_fields=('potential_energy',))
+mcmc.print_summary(exclude_deterministic=False)
+
+pe = mcmc.get_extra_fields()['potential_energy']
+print('Expected log joint density: {:.2f}'.format(np.mean(-pe))) 
+
+# import arviz as az
+# data = az.from_numpyro(mcmc)
+# az.plot_trace(data, compact=True, figsize=(15, 25))
